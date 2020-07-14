ec8fa7a935731fa43183ad4ea1fda61106a66bb9
Ahmed BENABDALLAH <hello@ahmedbenabdallah.com>
2020-07-14 17:51:38 +0200

    Add React Default Componant

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-console.log("Test");
+import React, { Component } from 'react';
 
+class index extends Component {
+    render() {
+        return (
+            <div>
+               < App/> 
+            </div>
+        );
+    }
+}
+
+export default index;
